b2165bab2835c92deb173d9a642c05ceb72326c5
Charles Young <young.charles90@gmail.com>
2021-04-22 14:23:57 -0500

    Fix kafka connection port number when running inside of docker

--- analytics-collector/src/config/kafkaConnection.js
+++ analytics-collector/src/config/kafkaConnection.js
@@ -1,7 +1,7 @@
 module.exports = {
     clientId: 'nodeJS-analytics-collector',
     kafka_topic: 'sessions-01',
-    brokers: ['0.0.0.0:9092'],
+    brokers: ['broker:29092'],
     connectionTimeout: 3000,
     authenticationTimeout: 1000,
     reauthenticationThreshold: 10000,
