4fe71eb101e2404564eea11709b7711eb3a67d34
Charles Young <young.charles90@gmail.com>
2021-05-02 18:06:46 -0500

    Add api key db check (untested)

--- analytics-collector/server.js
+++ analytics-collector/server.js
@@ -2,16 +2,23 @@
 
 const express = require('express');
 const {Kafka} = require('kafkajs');
-const config = require('./src/config/kafkaConnection');
+const kafkaConfig = require('./src/config/kafkaConnection');
 const bodyParser = require("body-parser");
 const cors = require('cors');
 const {checkSchema, validationResult} = require('express-validator');
 
 // Constants
 const PORT = 4100;
-const kafka = new Kafka(config);
+const kafka = new Kafka(kafkaConfig);
 const producer = kafka.producer();
 
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'me',
+    password: 'secret',
+    database: 'my_db'
+});
 
 const HOST = '0.0.0.0';
 const app = express();
@@ -87,30 +94,41 @@ app.post('/v1/track',
                 errorMessage: 'tracking event missing endpoint'
             }
         }
-    }), async (req, res) => {
+    }), (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({errors: errors.array()});
         }
 
-        const message = req.body
 
-        try {
-            await producer.connect()
-        } catch (e) {
-            res.sendStatus(503);
-        }
+        connection.connect();
 
-        try {
-            await producer.send({
-                topic: 'sessions-01',
-                messages:
-                    [{key: message.anonymousID, value: JSON.stringify(message)}],
-            });
-            res.status(200).json({status: 'message sent'});
-        } catch (e) {
-            res.status(500).json({[e.message]: e})
-        }
+        connection.query('SELECT 1 FROM assets WHERE api_token = ?', [req.body.apiKey],async (error, row) => {
+            if (error) throw error;
+            if (row.length) {
+                const message = req.body
+
+                try {
+                    await producer.connect()
+                } catch (e) {
+                    res.sendStatus(503);
+                }
+
+                try {
+                    await producer.send({
+                        topic: 'sessions-01',
+                        messages:
+                            [{key: message.anonymousID, value: JSON.stringify(message)}],
+                    });
+                    res.status(200).json({status: 'message sent'});
+                } catch (e) {
+                    res.status(500).json({[e.message]: e});
+                }
+            } else {
+                res.status(500).json({"error": "Bad api key"});
+            }
+        });
+        connection.end();
     });
 
 app.listen(PORT, HOST);
